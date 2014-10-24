17dfd8f4cb3616885c18baf88770f3db7e2b65eb
Ezequiel França <ezequiel.ifsp@gmail.com>
2014-10-24 10:06:43 -0200

    Initial Commit

---
+++ cidade-aumentada/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cidade-aumentada
+//
+//  Created by User on 24/10/14.
+//  Copyright (c) 2014 InovaCidades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ cidade-aumentada/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  cidade-aumentada
+//
+//  Created by User on 24/10/14.
+//  Copyright (c) 2014 InovaCidades. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
